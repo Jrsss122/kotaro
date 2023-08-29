d97c9dd230f25f5bdcf70ba3533c9017cf5719d5
sᴀᴍɪᴅᴀɪᴛᴇs <112374934+Samidaites2@users.noreply.github.com>
2023-08-29 22:15:01 +0700

    update YukkiMusic/core/userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -87,8 +87,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("heinoob")
-                await self.two.join_chat("store_ign")
+                await self.two.join_chat("humanimperfect")
+                await self.two.join_chat("zikastore907")
             except:
                 pass
             assistants.append(2)
@@ -117,8 +117,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("heinoob")
-                await self.three.join_chat("store_ign")
+                await self.three.join_chat("humanimperfect")
+                await self.three.join_chat("zikastore907")
             except:
                 pass
             assistants.append(3)
